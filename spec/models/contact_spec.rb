ca9f1b03aaff46b24e50bc35503166177ffca963
omz <omenkzz@gmail.com>
2016-08-01 02:16:21 +0700

    add validation Contact model

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Contact, type: :model do
   it "has a valid factory" do
     expect(build(:contact)).to be_valid
   end
+
+  it { is_expected.to validate_presence_of(:full_name) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_presence_of(:phone_number) }
+  it { is_expected.to validate_presence_of(:address) }
 end
